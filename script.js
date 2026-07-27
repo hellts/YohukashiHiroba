3eb83d4c2a958f66fc81add3556d431240b258eb
hellts <anatanobagu@gmail.com>
2026-07-27 16:12:12 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -48,3 +48,131 @@ Math.random()*5+"s";
 stars.appendChild(star);
 
 }
+
+// スクショ拡大
+
+document.querySelectorAll(".gallery-grid img")
+
+.forEach(img=>{
+
+img.onclick=()=>{
+
+window.open(img.src);
+
+}
+
+});
+
+// ----------------
+
+const track=document.querySelector(".carousel-track");
+
+document.querySelector(".right").onclick=()=>{
+
+track.scrollBy({
+
+left:track.clientWidth,
+
+behavior:"smooth"
+
+});
+
+};
+
+document.querySelector(".left").onclick=()=>{
+
+track.scrollBy({
+
+left:-track.clientWidth,
+
+behavior:"smooth"
+
+});
+
+};
+
+// ----------------
+
+const modal=document.createElement("div");
+
+modal.className="modal";
+
+modal.innerHTML="<img>";
+
+document.body.appendChild(modal);
+
+const modalImg=modal.querySelector("img");
+
+document.querySelectorAll(".carousel-track img")
+
+.forEach(img=>{
+
+img.onclick=()=>{
+
+modal.classList.add("active");
+
+modalImg.src=img.src;
+
+};
+
+});
+
+modal.onclick=()=>{
+
+modal.classList.remove("active");
+
+};
+
+// ----------------
+
+const topButton=document.getElementById("topButton");
+
+window.addEventListener("scroll",()=>{
+
+if(window.scrollY>400){
+
+topButton.style.display="block";
+
+}else{
+
+topButton.style.display="none";
+
+}
+
+});
+
+topButton.onclick=()=>{
+
+window.scrollTo({
+
+top:0,
+
+behavior:"smooth"
+
+});
+
+};
+
+// ----------------
+
+document.querySelectorAll("a[href^='#']")
+
+.forEach(anchor=>{
+
+anchor.addEventListener("click",function(e){
+
+e.preventDefault();
+
+document.querySelector(
+
+this.getAttribute("href")
+
+).scrollIntoView({
+
+behavior:"smooth"
+
+});
+
+});
+
+});
